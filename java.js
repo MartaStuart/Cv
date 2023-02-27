0e2781c704aa9e692aaae89b6ee385933ef1a844
MartaStuart <martincap@hotmail.com.ar>
2023-02-27 18:03:09 -0300

    subimos proyecto final

---
+++ java.js
@@ -0,0 +1,6 @@
+ function funcionemail() {
+    document.getElementById ("pemail").innerHTML ="federicocarreras@hotmail.com"
+ }
+ function funcionnumero() {
+   document.getElementById ("pnumero").innerHTML ="341-3087999"
+ }
